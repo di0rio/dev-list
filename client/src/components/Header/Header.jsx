04372421f790e386b6939b95051d2a6189d662de
di0rio <caua.diorio29@gmail.com>
2023-12-07 08:23:43 -0300

    imagens adicionadas ao projeto

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        
+      </div>
+    </header>
+  );
+};
+
+export default Header;
