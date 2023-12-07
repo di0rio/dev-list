48b188d5ad9b5db6730ea0cc1924028ec2b12fa2
di0rio <caua.diorio29@gmail.com>
2023-12-07 09:10:47 -0300

    header finalizado

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,10 +1,31 @@
 import styles from "./Header.module.css";
 
+import linkedin from "../../assets/logo-linkedin.svg";
+import github from "../../assets/logo-github.svg";
+import telegram from "../../assets/logo-telegram.svg";
+
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        
+        <h1 className={styles.title}>To-Do List</h1>
+        <ul className={styles.socialMedia}>
+          <li>
+            <a className={styles.link} href="https://linkedin.com">
+              <img src={linkedin} alt="" />
+            </a>
+          </li>
+          <li>
+            <a className={styles.link} href="https://github.com">
+              <img src={github} alt="" />
+            </a>
+          </li>
+          <li>
+            <a className={styles.link} href="https://t.me">
+              <img src={telegram} alt="" />
+            </a>
+          </li>
+        </ul>
       </div>
     </header>
   );
